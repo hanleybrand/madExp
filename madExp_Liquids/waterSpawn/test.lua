fa0a338860066933ed9109075fc73ea6efd64e8c
Mr. Kal <xpiratosx@gmail.com>
2015-04-11 13:47:02 +0530

    Debug 1
    Debug

--- madExp_Liquids/waterSpawn/test.lua
+++
@@ -1,9 +0,0 @@
-function init()
-entity.setInteractive(entity.configParameter("interactive", true))
-end
-
-function onInteraction(args)
-	local pos = entity.position()
-  local modifiedPosition = {pos[1], pos[2]-4}
-	world.spawnLiquid( modifiedPosition, 1, 100)
-end
