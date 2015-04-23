8bab7e3e031829417ad96c5db0ade3e837df5274
Mr. Kal <xpiratosx@gmail.com>
2015-04-23 12:09:21 +0530

    Hotfix for Pet Spawning
    Pet spawning had a few massive bugs, fixed hopefully now. Also added a
    readme.

--- madExp_RandomPet/objects/spawner/shippetspawner.lua
+++ madExp_RandomPet/objects/spawner/shippetspawner.lua
@@ -3,9 +3,18 @@ function init()
   storage.spawnTimer = storage.spawnTimer and 0.5 or 0
   storage.petParams = storage.petParams or {}
   pets = { "petbunny", "petcat", "crasberry", "petorbis", "piglett", "petsnake", "snugget", "petweasel" }	
-  local x = math.random(1,8)
-	self.monsterType = pets[x]
+	if storage.monsterType == nil then 
+		local i = randomizer()
+		self.monsterType = pets[i]
+		storage.monsterType = self.monsterType
+	end
   self.spawnOffset = entity.configParameter("spawnOffset", {0, 2})
+end
+
+function randomizer()
+	math.randomseed(os.time())
+	randomNum = math.random(1,8)
+	return randomNum
 end
 
 function hasPet()
@@ -28,7 +37,7 @@ function update(dt)
 
   if storage.spawnTimer < 0 and self.petId == nil then
     storage.petParams.level = 1
-    self.petId = world.spawnMonster(self.monsterType, entity.toAbsolutePosition(self.spawnOffset), storage.petParams)
+    self.petId = world.spawnMonster(storage.monsterType, entity.toAbsolutePosition(self.spawnOffset), storage.petParams)
     world.callScriptedEntity(self.petId, "setAnchor", entity.id())
     storage.spawnTimer = 0.5
   else
